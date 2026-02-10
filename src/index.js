009516227b9054a799109f271699a787ca1404f3
emilia <187101155+emilia0804@users.noreply.github.com>
2026-02-10 19:38:00 +0630

    Initital commit full version

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import "./style.css";
+import { renderAbout } from "./pages/about.js";
+import { renderHome } from "./pages/home.js";
+import { renderMenu } from "./pages/menu.js";
+
+renderHome();
+
+const nav = document.querySelector("nav");
+
+nav.addEventListener("click", e => {
+    switch(e.target.id) {
+        case "home":
+            renderHome();
+            break;
+        case "menu":
+            renderMenu();
+            break;
+        case "about":
+            renderAbout();
+            break;
+    }
+});
